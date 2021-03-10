477ed7c2043b452046d99ebd647ac3d55c695617
lizining <lizining@ztgame.com>
2021-03-10 15:11:27 +0800

    增加快排

---
+++ 快速排序/Solution.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public void QuickSort(int[] nums)
+    {
+        qs(nums, 0, nums.Length - 1);
+    }
+    private void Qs(int[] nums, int l, int r)
+    {
+        if(l<r){
+            int index = Partition(nums, l, r);
+            Qs(nums, l, index-1);
+            Qs(nums, index+1, r);
+        }
+    }
+
+    private int Partition(int[] nums, int l, int r){
+        int key = nums[l];
+        while(l<r){
+            while(l<r&&nums[r]>=key)r--;
+            nums[l]=nums[r];
+            while(l<r&&nums[l]<=key)l++;
+            nums[r]=nums[l];
+        }
+        nums[l]=key;
+        return l;
+    }
+}
